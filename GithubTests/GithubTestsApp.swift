ccda93637de9317d8adc23f9960a7ef8b4297deb
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-01-27 11:54:26 +1300

    Initial Commit

---
+++ GithubTests/GithubTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestsApp.swift
+//  GithubTests
+//
+//  Created by Flynn Stevens on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
